a843ee9c06c2f19bfd1f0a7260c929c14bfdd0f6
Rudy Tantono <rudzlong@gmail.com>
2022-09-11 13:15:23 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,29 +1,16 @@
 const Discord = require('discord.js');
-
 const client = new Discord.Client({intents: ["GUILDS", "GUILD_MESSAGES"]});
 
  
-
 client.on('ready', () => {
-
-    console.log('I am ready!');
-
+  console.log(`Logged in as ${client.user.tag}!`);
+  client.user.setActivity('Your Heart ❤️');
 });
 
- 
-
-client.on('message', message => {
-
-    if (message.content === 'ping') {
-
-       message.reply('pong');
-
-       }
-
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('pong!');
+  }
 });
 
- 
-
-// THIS  MUST  BE  THIS  WAY
-
-client.login(process.env.BOT_TOKEN);//where BOT_TOKEN is the token of our bot
+client.login(process.env.BOT_TOKEN);
